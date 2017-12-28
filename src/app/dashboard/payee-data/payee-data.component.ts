b64745fdbdf76cf62451a13422e3a7c4e946c33a
Saeed Ansari <saedansari@gmail.com>
2017-12-28 00:51:49 -0500

    payee address sections changes

--- src/app/dashboard/payee-data/payee-data.component.ts
+++ src/app/dashboard/payee-data/payee-data.component.ts
@@ -1,15 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { UserService } from '../../services/user.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { PayeeService } from '../../services/payee.service';
 
 @Component({
   selector: 'app-payee-data',
   templateUrl: './payee-data.component.html',
-  styleUrls: ['./payee-data.component.css']
+  styleUrls: ['./payee-data.component.css'],
+  providers: [PayeeService, UserService]
 })
+
 export class PayeeDataComponent implements OnInit {
 
-  constructor() { }
+  @Input() UserID = 0;
+  private lstPayees = [];
+
+  constructor(
+    private payeeService: PayeeService,
+    private userService: UserService,
+    private route: ActivatedRoute,
+    public router: Router) { }
 
   ngOnInit() {
+    this.UserID = this.userService.getLoggedInUserID();
+    this.UserID = 15;
+    this.loadPayees();
   }
 
+  loadPayees() {
+    this.lstPayees = [];
+    const payeesUnfilteres = [];
+    this.payeeService.GetPayees(this.UserID, '').subscribe(sub => {
+      sub.forEach(element => {
+        payeesUnfilteres.push({
+          ID: element.ID,
+          Name: element.Name,
+          AccountNumber: element.AccountNumber
+        });
+      });
+
+      this.lstPayees = payeesUnfilteres.filter(item => item.AccountNumber != null);
+    });
+  }
 }
